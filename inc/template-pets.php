801c9aa9da187a7a1d0920d4835c0dc0c84c34ae
Alexey Demyanov <deal110988@him-mail.ch>
2025-05-22 16:11:53 +0200

    finished with the functionality

--- inc/template-pets.php
+++ inc/template-pets.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 <div class="container container--narrow page-section">
 
-  <p>This page took <strong><?php echo timer_stop(); ?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
+  <p>This page took <strong><?php echo timer_stop(); ?></strong> seconds to prepare. Found <strong><?php echo number_format($getPets->count) ?></strong> results (showing the first <?php echo count($getPets->pets) ?>).</p>
 
   <table class="pet-adoption-table">
     <tr>
@@ -28,6 +28,9 @@ get_header(); ?>
       <th>Hobby</th>
       <th>Favorite Color</th>
       <th>Favorite Food</th>
+      <?php if (current_user_can('administrator')) ?>
+      <th>Delete</th>
+      <?php ?>
     </tr>
     <?php foreach ($getPets->pets as $pet) {
     ?> <tr>
@@ -38,9 +41,35 @@ get_header(); ?>
         <td><?php echo $pet->favhobby ?></td>
         <td><?php echo $pet->favcolor ?></td>
         <td><?php echo $pet->favfood ?></td>
+        <?php if (current_user_can('administrator')) ?>
+        <td style="text-align: center">
+          <form action="<?php echo esc_url(admin_url('admin-post.php')) ?>" method="POST">
+            <input type="hidden" name="action" value="deletepet">
+            <input type="hidden" name="idtodelete" value="<?php echo $pet->id ?>">
+            <button class="delete-pet-button">X</button>
+          </form>
+        </td>
+        <?php ?>
       </tr> <?php
           } ?>
   </table>
+
+  <!-- When posting to admin-post.php, WP will see name="action" field
+          and will use the value of this field ("createpet") to create a special hook
+          to be able to hook onto in the main plugin file (new-database-table.php)
+    -->
+
+  <?php if (current_user_can('administrator')) { ?>
+    <!-- admin-post.php is a special WP file for this exact purpose -->
+    <form action="<?php echo esc_url(admin_url('admin-post.php')) ?>" class="create-pet-form" method="POST">
+      <p>Enter name for new pet</p>
+      <!-- name="action" is special reserved name and has to be used here -->
+      <input type="hidden" name="action" value="createpet">
+      <input type="text" name="incomingpetname" placeholder="name...">
+      <button>Add pet</button>
+    </form>
+  <?php
+  } ?>
 
 </div>
 
